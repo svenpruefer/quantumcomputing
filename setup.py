3e0a652f907fbd487d7b71b5ef3d1e08db97434f
Sven Prüfer <sven.pruefer@dlr.de>
2020-03-27 12:01:31 +0100

    Add basic setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quantumcomputing',
+    version='0.1.0',
+    packages=['quantumcomputing', 'quantumcomputing.tests'],
+    url='https://github.com/svenpruefer/quantumcomputing',
+    license='',
+    author='Sven Prüfer',
+    author_email='sven.pruefer@dlr.de',
+    description=''
+)
