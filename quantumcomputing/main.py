a19059fd81203b34edbb96a8308e8456dbed3541
Sven Prüfer <sven.pruefer@dlr.de>
2020-03-27 12:02:13 +0100

    Add basic project structure

---
+++ quantumcomputing/main.py
@@ -0,0 +1,13 @@
+#  Copyright (c) 2020 by DLR.
+
+
+def main():
+    """
+    Main entry point for quantumcomputing.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
