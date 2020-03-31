869ede8a1a5db8fd53d63d0b64602575bdb3f7cd
Sven Prüfer <sven.pruefer@dlr.de>
2020-03-31 11:58:19 +0200

    Implement Grover with ancilla bit to search for |10> including unit test

--- quantumcomputing/gates/grover.py
+++ quantumcomputing/gates/grover.py
@@ -5,7 +5,8 @@
 # Copyright (c) 2020 by DLR.
 
 from qiskit import QuantumCircuit
-from qiskit.circuit import Qubit
+from qiskit.circuit import Qubit, Clbit
+
 
 def add_grover_without_ancilla_1_0(qc: QuantumCircuit, q_0: Qubit, q_1: Qubit) -> None:
     # Oracle
@@ -32,6 +33,33 @@ def add_grover_without_ancilla_1_0(qc: QuantumCircuit, q_0: Qubit, q_1: Qubit) -
     qc.barrier()
 
 
-
-
-
+def add_grover_with_ancilla_1_0(qc: QuantumCircuit, q_0: Qubit, q_1: Qubit, ancilla: Qubit, measure_ancilla: Clbit) -> None:
+    # Setup
+    qc.x(ancilla)
+    qc.h(q_0)
+    qc.h(q_1)
+    qc.h(ancilla)
+    qc.barrier()
+    # Oracle
+    qc.x(q_0)
+    qc.ccx(q_1, q_0, ancilla)
+    qc.x(q_0)
+    qc.measure(ancilla, measure_ancilla)
+    qc.barrier()
+    # Mix
+    qc.h(q_0)
+    qc.h(q_1)
+    qc.barrier()
+    # Reflection
+    qc.x(q_1)
+    qc.x(q_0)
+    qc.h(q_0)
+    qc.cx(q_1, q_0)
+    qc.h(q_0)
+    qc.x(q_1)
+    qc.x(q_0)
+    qc.barrier()
+    # Mix
+    qc.h(q_0)
+    qc.h(q_1)
+    qc.barrier()
