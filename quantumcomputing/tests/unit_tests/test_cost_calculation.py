d2af8846eb561504a61e23b646d6c9bd02119dc8
Sven Prüfer <sven.pruefer@dlr.de>
2020-03-31 12:31:56 +0200

    Test cost calculations for Grover implementations with and without ancilla bits

---
+++ quantumcomputing/tests/unit_tests/test_cost_calculation.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of quantumcomputing.
+#
+# Copyright (c) 2020 by DLR.
+
+from typing import *
+
+import pytest
+from pytest import approx
+from qiskit import *
+from qiskit.providers import *
+
+from costs.costs import calc_total_costs
+from quantumcomputing.gates.grover import add_grover_without_ancilla_1_0, add_grover_with_ancilla_1_0
+
+class TestCostCalculation:
+
+    def test_cost_grover_without_ancilla(self):
+        #Given
+        qreg = QuantumRegister(2, "qreg")
+        creg = ClassicalRegister(2, "creg")
+        qc = QuantumCircuit(qreg, creg, name="grover-without-ancilla-circuit")
+
+        # Mix states
+        qc.h(qreg)
+        # Add one Grover step
+        add_grover_without_ancilla_1_0(qc, qreg[0], qreg[1])
+        # Measure
+        qc.measure(qreg[0], creg[0])
+        qc.measure(qreg[1], creg[1])
+
+        # When
+        result = calc_total_costs(qc)
+
+        # Then
+        assert result == 36
+
+    def test_cost_grover_with_ancilla(self):
+        #Given
+        qreg = QuantumRegister(2, "input")
+        ancilla = QuantumRegister(1, "ancilla")
+        measure = ClassicalRegister(2, "measure")
+        measure_ancilla = ClassicalRegister(1, "ancilla-measure")
+        qc = QuantumCircuit(qreg, ancilla, measure, measure_ancilla, name="grover-without-ancilla-circuit")
+
+        # Add one Grover step
+        add_grover_with_ancilla_1_0(qc, qreg[0], qreg[1], ancilla[0], measure_ancilla[0])
+        # Measure
+        qc.measure(qreg[0], measure[0])
+        qc.measure(qreg[1], measure[1])
+
+        # When
+        result = calc_total_costs(qc)
+
+        # Then
+        assert result == 95
