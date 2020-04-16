6c210ae49bb8e47df40663ff4f66f1dd8dca7234
Sven Prüfer <sven.pruefer@dlr.de>
2020-04-16 17:13:40 +0200

    Add Graph method to construct a quantum circuit from graph and fix some wrong method usages

--- quantumcomputing/graph/graph.py
+++ quantumcomputing/graph/graph.py
@@ -6,7 +6,9 @@
 
 from typing import Set, Dict, Tuple
 
-from quantumcomputing.circuits.coloring import VertexColor
+from qiskit import QuantumCircuit, QuantumRegister
+
+from quantumcomputing.circuits.coloring import VertexColor, add_4_coloring_grover
 
 
 class Graph:
@@ -15,7 +17,7 @@ class Graph:
     _external_vertices: Set[str] = set()
     _edges: Set[Tuple[str, str]] = set()
     _internal_edges: Set[Tuple[str, str]] = set()
-    _external_edges: Set[Tuple[str, VertexColor]] = set()
+    _external_edges: Set[Tuple[str, str]] = set()
     _colors: Dict[str, VertexColor] = {}
 
     def __init__(self, vertices: Set[str], edges: Set[Tuple[str, str]],
@@ -41,9 +43,9 @@ class Graph:
         self._vertices = vertices
         for vertex in vertices:
             if vertex in given_colors.keys():
-                self._external_vertices += vertex
+                self._external_vertices.add(vertex)
             else:
-                self._internal_vertices += vertex
+                self._internal_vertices.add(vertex)
         # Separate edges
         self._edges = edges
         for edge in edges:
@@ -53,10 +55,89 @@ class Graph:
                         f"Invalid input as the given colors are inconsistent since {edge[0]} and {edge[1]}" +
                         f" have an identical color {given_colors[edge[0]]}")
             elif edge[0] in given_colors.keys():
-                self._external_edges += edge
+                self._external_edges.add(edge)
                 self._colors[edge[0]] = given_colors[edge[0]]
             elif edge[1] in given_colors.keys():
-                self._external_edges += edge
+                self._external_edges.add(edge)
                 self._colors[edge[1]] = given_colors[edge[1]]
             else:
-                self._internal_edges += edge
+                self._internal_edges.add(edge)
+
+        print(f"Created graph with {len(self._internal_vertices)} uncolored vertices,"
+              f" {len(self._internal_edges)} internal edges and {len(self._external_edges)} external edges")
+
+    def get_4_color_grover_circuit(self, repetitions: int = 5) -> QuantumCircuit:
+        """
+        Create a 4-color Grover quantum circuit with 'repetitions' many repetitions for this graph.
+
+        Vertex quantum registers are named as 'v-<name>', where <name> is the name of the vertex and
+        the ancilla qubit is called 'ancilla'.
+
+        :return: Quantum Circuit with 4-color Grover circuit
+        """
+        # Create quantum registers for vertices
+        vertices: Dict[str, QuantumRegister] = {name: QuantumRegister(2, f"v-{name}") for name in
+                                                self._internal_vertices}
+
+        # Create quantum register for ancilla qubit
+        ancilla: QuantumRegister = QuantumRegister(1, "ancilla")
+
+        # Determine colors for external edges
+        external_edges: Set[Tuple[str, VertexColor]] = set()
+        for v1, v2 in self._external_edges:
+            if v1 in self._colors.keys():
+                external_edges.add((v2, self._colors[v1]))
+            if v2 in self._colors.keys():
+                external_edges.add((v1, self._colors[v2]))
+
+        # Determine how many target qubits are needed and create suitable quantum register
+        # We separate internal and external edges into groups of four and add one qubit for
+        # possibly remaining internal and external edges
+        internal_number_4_groups, internal_remainder = divmod(len(self._internal_edges), 4)
+        external_number_4_groups, external_remainder = divmod(len(external_edges), 4)
+        total_number_target_qubits = internal_number_4_groups + external_number_4_groups
+        if internal_remainder > 0:
+            total_number_target_qubits += 1
+        if external_remainder > 0:
+            total_number_target_qubits += 1
+        target: QuantumRegister = QuantumRegister(total_number_target_qubits, "target")
+
+        # Determine how many auxiliary qubits are needed and create suitable quantum register
+        # The number of auxiliary qubits needed is the maximum number of necessary ancilla qubits
+        # for any of the used operations. The needs are as follows:
+        # - internal 4 edges: 6
+        # - internal 3 edges: 4
+        # - internal 2 edges: 2
+        # - internal 1 edge: 0
+        # - external 4 edges: 6
+        # - external 3 edges: 4
+        # - external 2 edges: 2
+        # - external 1 edge: 0
+        # - Multi-Toffoli-gate for combining the target qubits via AND: #target - 2
+        # - Grover reflection: 2 * #vertices - 3 - #target
+        #   (Because after the Grover oracle step, the target qubits are |0> and can be used as ancilla
+        #   qubits for the Grover reflection)
+        total_number_auxiliary_qubits = max(6,
+                                            total_number_target_qubits - 2,
+                                            2 * len(vertices) - 3 - total_number_target_qubits
+                                            )
+        auxiliary: QuantumRegister = QuantumRegister(total_number_auxiliary_qubits, "auxiliary")
+
+        # Create QuantumCircuit including all quantum registers
+        qc: QuantumCircuit = QuantumCircuit(name="four-color-grover-circuit")
+        for register in vertices.values():
+            qc.add_register(register)
+        qc.add_register(auxiliary)
+        qc.add_register(target)
+        qc.add_register(ancilla)
+
+        print(
+            f"Created quantum circuit for graph with {len(vertices)} vertex registers,"
+            f" {total_number_auxiliary_qubits} auxiliary qubits,"
+            f" {total_number_target_qubits} target qubits and {len(list(ancilla))} ancilla qubits")
+
+        # Add 4-color Grover circuit
+        add_4_coloring_grover(qc, vertices, self._internal_edges, external_edges, auxiliary, target, ancilla[0],
+                              repetitions)
+
+        return qc
